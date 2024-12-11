3d92164c6b80a9cbabc4785a074c6424ab40c962
Román González <roman.montano12@gmail.com>
2024-12-10 23:33:17 -0600

    🎨Layout principal creado

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue';
+</script>
+
+<template>
+  <main class="bg-[#F0F7FF] min-h-screen">
+    <Header />
+
+    <section class="pt-24 container mx-auto">
+      <router-view />
+    </section>
+  </main>
+</template>
