1a3a82b3ed1d797b6448209a078143f22d1eb531
Román González <roman.montano12@gmail.com>
2024-12-10 23:32:34 -0600

    ✨Agregando componenete del header

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useImageStore } from '@/stores/imageStore';
+
+const query = ref('');
+const router = useRouter();
+const imageStore = useImageStore();
+
+const search = () => {
+  imageStore.searchImages(query.value);
+  router.push({ name: 'SearchResults' });
+};
+</script>
+
+<template>
+  <header class="fixed w-full">
+    <div class="container mx-auto py-4 flex justify-between space-x-8 items-center">
+      <router-link to="/explore">
+        <img class="w-36" src="@/assets/images/Logo.png" alt="Logo">
+      </router-link>
+
+      <input v-model="query"
+        @keydown.enter="search"
+        placeholder="Buscar imágenes..."
+        class="w-full p-2 rounded"
+      />
+    </div>
+  </header>
+</template>
