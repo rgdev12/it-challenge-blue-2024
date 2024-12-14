ba45695112c3c72917147ff35fc12e69b600b0b9
Román González <roman.montano12@gmail.com>
2024-12-14 13:48:09 -0600

    🔥Removiendo función que no se utiliza del image service

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,21 +1,11 @@
 import apiClient from '@/http-common';
 import type  { ImageSearchParams, ImageSearchResponse } from '@/utils/intefaces/ImageInterfaces'
 
-const searchImages = async (query: string) => {
-  const response = await apiClient.get('/images/search', {
-    params: {
-      query,
-    },
-  });
-  return response.data;
-};
-
 const getImagesByParams = async (params: ImageSearchParams): Promise<ImageSearchResponse> => {
   const response = await apiClient.post('/photos', params);
   return response.data;
 };
 
 export default {
-  searchImages,
   getImagesByParams
 };
